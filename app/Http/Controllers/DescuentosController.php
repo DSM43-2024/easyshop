9465b4e7376aa6fa48028c27caae793e1c0cd026
Cloudy <cloudysa139@gmail.com>
2024-10-14 18:47:33 -0600

    actualizacion 0.4 controlador descuentos

--- app/Http/Controllers/DescuentosController.php
+++ app/Http/Controllers/DescuentosController.php
@@ -1,62 +1,64 @@
 <?php
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
-use Auth;
 use Illuminate\Support\Facades\Session;
 use App\Models\Descuentos;
+
 class DescuentosController extends Controller
 {
-    public function carreras(){
+    public function descuentos(){
         return view('descuentos')
-        ->with(['carreras'=> Descuentos::all()]);
+        ->with(['descuentos'=> Descuentos::all()]);
+    }
 
+    public function descuento_alta()  {
+        return view("descuento_alta");
     }
-    public function carrera_alta()  {
-        return view ("carrera_alta");
-    }
-    public function carrera_registrar(Request $request){
-        $this->validate($request,[
-            'clave'=> 'required',
-            'nombre'=>'required',
-            'detalle'=>'required',
-            'activo'=>'required'
+
+    public function descuento_registrar(Request $request){
+        $this->validate($request, [
+            'clave' => 'required',
+            'nombre' => 'required',
+            'detalle' => 'required',
+            'activo' => 'required'
         ]);
-        $carrera = new Carreras();
-        $carrera->clave = $request->input('clave');
-        $carrera->nombre = $request->input('nombre');
-        $carrera->detalle = $request->input('detalle');
-        $carrera->activo = $request->input('activo');
-        $carrera->save();
-    
-        return redirect()->route('carreras')->with('success', 'registrado exitosamente');
+
+        $descuento = new Descuentos();
+        $descuento->clave = $request->input('clave');
+        $descuento->nombre = $request->input('nombre');
+        $descuento->detalle = $request->input('detalle');
+        $descuento->activo = $request->input('activo');
+        $descuento->save();
+
+        return redirect()->route('descuentos')->with('success', 'Registrado exitosamente');
     }
- public function carrera_detalle($id){
-    $query=Carreras::find($id);
-    return view("carrera_detalle")
-    ->with(['carrera'=>$query]);
- }
-    
-    
-    public function carrera_editar($id){
-        $query=Carreras::find($id);
-        return view('carrera_editar')
-        -> with(['carrera'=> $query ]);
+
+    public function descuento_detalle($id){
+        $query = Descuentos::find($id);
+        return view("descuento_detalle")
+        ->with(['descuento' => $query]);
     }
-    public function carrera_salvar(Carreras $id, Request $request) {
-        $query = Carreras::find($id->id_carrera);
+
+    public function descuento_editar($id){
+        $query = Descuentos::find($id);
+        return view('descuento_editar')
+        ->with(['descuento' => $query]);
+    }
+
+    public function descuento_salvar(Descuentos $id, Request $request) {
+        $query = Descuentos::find($id->id_descuento);
         $query->clave = $request->clave;
-        $query->nombre= $request->nombre;    
+        $query->nombre = $request->nombre;    
         $query->detalle = $request->detalle;    
-        $query->activo= $request->activo;    
+        $query->activo = $request->activo;    
         $query->save();
-    
-        return redirect()->route("carrera_editar", ['id' => $id->id_carrera]);
+
+        return redirect()->route("descuento_editar", ['id' => $id->id_descuento]);
     }
-    
-    public function carrera_borrar(Carreras $id){
+
+    public function descuento_borrar(Descuentos $id){
         $id->delete();
-        return redirect()->route('carreras');
+        return redirect()->route('descuentos');
     }
 }
-
-
